265578ee9530b8e6e5e553b2f52a81bd01de9927
prateek145 <prateekk898@gmail.com>
2023-08-12 16:06:51 +0530

    local

--- app/Http/Controllers/api/LoginController.php
+++ app/Http/Controllers/api/LoginController.php
@@ -52,7 +52,7 @@ class LoginController extends Controller
                     @[, $file_data] = explode(',', $file_data);
                     $imageName = Str::random(10) . '.' . 'png';
                     Storage::disk('local')->put($imageName, base64_decode($file_data));
-                    dd($imageName);
+                    dd($imageName, $imageName, base64_decode($file_data));
 
                     $data['image'] = $imageName;
                 }
