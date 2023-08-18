6b22adb79e26e2fba2fbb013e831090bcd821c74
prateek145 <prateekk898@gmail.com>
2023-08-18 17:55:07 +0530

    local

--- app/Http/Controllers/api/LoginController.php
+++ app/Http/Controllers/api/LoginController.php
@@ -55,7 +55,7 @@ class LoginController extends Controller
                     $data['image'] = $imageName;
                 }
 
-                dd($data);
+                // dd($data);
                 $user = User::create($data);
                 return response()
                     ->json([
